6eab031bd9f588131ace9cca2b40e901f349fb5a
Prachi Sablani <sablaniprachi@gmail.com>
2020-06-16 23:38:11 -0700

    Create User Activity for the Past 30 Days II.sql

---
+++ User Activity for the Past 30 Days II.sql
@@ -0,0 +1,21 @@
+/*
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| user_id       | int     |
+| session_id    | int     |
+| activity_date | date    |
+| activity_type | enum    |
++---------------+---------+
+There is no primary key for this table, it may have duplicate rows.
+The activity_type column is an ENUM of type ('open_session', 'end_session', 'scroll_down', 'send_message').
+The table shows the user activities for a social media website.
+Note that each session belongs to exactly one user.
+ 
+
+Write an SQL query to find the average number of sessions per user for a period of 30 days ending 2019-07-27 inclusively, rounded to 2 decimal places. The sessions we want to count for a user are those with at least one activity in that time period.
+*/
+
+SELECT ifnull(ROUND(COUNT(DISTINCT session_id)/COUNT(DISTINCT user_id), 2),0.00) AS 'average_sessions_per_user'
+FROM Activity
+WHERE activity_date > SUBDATE('2019-07-27', INTERVAL 30 DAY)
