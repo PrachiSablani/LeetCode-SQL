be486004da111d5fc07d6ac88d7d4ff2a632af23
Prachi Sablani <sablaniprachi@gmail.com>
2020-06-17 20:53:42 -0700

    Create Project Employees II.sql

---
+++ Project Employees II.sql
@@ -0,0 +1,30 @@
+/*
+Table: Project
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| project_id  | int     |
+| employee_id | int     |
++-------------+---------+
+(project_id, employee_id) is the primary key of this table.
+employee_id is a foreign key to Employee table.
+Table: Employee
+
++------------------+---------+
+| Column Name      | Type    |
++------------------+---------+
+| employee_id      | int     |
+| name             | varchar |
+| experience_years | int     |
++------------------+---------+
+employee_id is the primary key of this table.
+ 
+
+Write an SQL query that reports all the projects that have the most employees.
+*/
+
+SELECT project_id
+FROM Project
+GROUP BY project_id
+HAVING COUNT(employee_id) = (SELECT count(employee_id) FROM Project GROUP BY project_id ORDER BY 1 DESC LIMIT 1)
