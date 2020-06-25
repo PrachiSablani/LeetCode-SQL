1e9ab727d62d97ca8f0d23180c43742945737193
Prachi Sablani <sablaniprachi@gmail.com>
2020-06-25 02:04:05 -0700

    Create Second Degree Follower.sql

---
+++ Second Degree Follower.sql
@@ -0,0 +1,28 @@
+/*
+In facebook, there is a follow table with two columns: followee, follower.
+
+Please write a sql query to get the amount of each follower’s follower if he/she has one.
+
+For example:
+
++-------------+------------+
+| followee    | follower   |
++-------------+------------+
+|     A       |     B      |
+|     B       |     C      |
+|     B       |     D      |
+|     D       |     E      |
++-------------+------------+
+should output:
++-------------+------------+
+| follower    | num        |
++-------------+------------+
+|     B       |  2         |
+|     D       |  1         |
++-------------+------------+
+*/
+
+SELECT a.follower, COUNT(DISTINCT b.follower) AS num
+FROM follow a JOIN follow b
+ON a.follower = b.followee
+GROUP BY a.follower
